bcfcc6d8ac17bf2b55fc408a5d27df4068c4366f
Jarren San Jose <sanjose.jarren@gmail.com>
2025-08-02 03:01:44 -0700

    add short description for the website

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <main className="min-h-screen p-8 flex flex-col items-center justify-center text-center">
       <h1 className="text-2xl font-bold mb-4">kwaji&apos;s design portfolio</h1>
+      <p className="text-lg mb-4">
+        I&apos;m a software engineer. This is my design portfolio.
+      </p>
     </main>
   );
 }
